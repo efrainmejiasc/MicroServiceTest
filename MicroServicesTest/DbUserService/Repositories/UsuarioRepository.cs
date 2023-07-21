4cd9e63e3830d70dae76a8782884454d8bec124e
efrain.mejias@dirmod.com <efrain.mejias@dirmod.com>
2023-07-20 20:17:59 -0400

    RepoUsuario

---
+++ MicroServicesTest/DbUserService/Repositories/UsuarioRepository.cs
@@ -0,0 +1,66 @@
+﻿using DbUserService.IRepositories;
+using DbUserService.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbUserService.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly DbUserContext _dbUserContext;
+        public UsuarioRepository(DbUserContext dbUserContext)
+        {
+            this._dbUserContext = dbUserContext;
+        }
+
+        public async Task<Usuario> AddUsuarioAsync(Usuario x)
+        {
+            await this._dbUserContext.Usuario.AddAsync(x);
+            await this._dbUserContext.SaveChangesAsync();
+
+            return x;
+        }
+
+        public Usuario GetUsuario(string key)
+        {
+            return this._dbUserContext.Usuario.Where(x => x.KeyUsuario == key).FirstOrDefault();
+        }
+
+        public async Task<Usuario> UpdateUsuarioAsync(Usuario x)
+        {
+            var usuario = await this._dbUserContext.Usuario.FindAsync(x.Id);
+
+            if (usuario != null)
+            {
+                usuario.Nombre = x.Nombre;
+                usuario.Apellido = x.Apellido;
+                usuario.TipoDocumento = x.TipoDocumento;
+                usuario.Documento = x.Documento;
+                usuario.KeyUsuario = x.KeyUsuario;
+
+                await this._dbUserContext.SaveChangesAsync();
+            }
+
+            return usuario;
+        }
+
+        public async Task<bool> DeleteUsuarioAsync(int userId)
+        {
+            var result = false;
+            var usuario = await this._dbUserContext.Usuario.FindAsync(userId);
+
+            if (usuario != null)
+            {
+                this._dbUserContext.Usuario.Remove(usuario);
+                await this._dbUserContext.SaveChangesAsync();
+                result = true; 
+            }
+
+            return result;
+        }
+    }
+}
